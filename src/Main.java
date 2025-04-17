98309f0fea07da6b3887ce0a376675d5a8e744b8
David <davidstupen@seznam.cz>
2025-04-17 16:44:17 +0200

    úkol prodejce mrkve

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Sellers seller1 = new Sellers("Vojta", 12, 210, 28.3, true, "Masarykova 18, 754 36 Praha");
+        Sellers seller2 = new Sellers("David", 9, 180, 22.65, false, "Školní 25, 251 55 Plzeň");
+        displey(seller1);
+        displey(seller2);
+    }
+    static void displey(Sellers seller){
+        System.out.println(seller.name);
+    }
+}
---
+++ src/Sellers.java
@@ -0,0 +1,16 @@
+public class Sellers {
+    String name;
+    int dateBirth;
+    int numOfDeal;
+    double totalSellTon;
+    boolean seniorSell;
+    String address;
+    Sellers(String name, int dateBirth, int numOfDeal, double totalSellTon, boolean seniorSell, String address){
+        this.name = name;
+        this.dateBirth = dateBirth;
+        this.numOfDeal = numOfDeal;
+        this.totalSellTon = totalSellTon;
+        this.seniorSell = seniorSell;
+        this.address = address;
+    }
+}
